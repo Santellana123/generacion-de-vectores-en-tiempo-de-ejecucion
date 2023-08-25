8d4f6d0ac457b743867a1299dab4f9454faf95a4
Santellana179 <imjuan179@gmail.com>
2023-08-25 03:18:01 -0500

    Agregar archivos de proyecto.

---
+++ generacion de vectores en tiempo de ejecucion/Program.cs
@@ -0,0 +1,33 @@
+﻿int longitud;
+Console.Write("Ingrese la longitud del vector: ");
+longitud = int.Parse(Console.ReadLine());
+
+int[] vector = GenerarVector(longitud);
+
+Console.WriteLine("El vector generado es:");
+ImprimirVector(vector);
+
+Console.ReadKey();
+       
+
+        static int[] GenerarVector(int longitud)
+{
+    Random random = new Random();
+    int[] vector = new int[longitud];
+
+    for (int i = 0; i < longitud; i++)
+    {
+        vector[i] = random.Next(1, 100); 
+    }
+
+    return vector;
+}
+
+static void ImprimirVector(int[] vector)
+{
+    foreach (int num in vector)
+    {
+        Console.Write(num + " ");
+    }
+    Console.WriteLine();
+   }
